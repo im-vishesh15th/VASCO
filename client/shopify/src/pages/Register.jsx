009f3fdab060ffa7820d76f54d5b97940cede1a4
im-vishesh15th <145531271+im-vishesh15th@users.noreply.github.com>
2024-03-28 19:26:47 +0530

    Update Register.jsx

--- client/shopify/src/pages/Register.jsx
+++ client/shopify/src/pages/Register.jsx
@@ -12,7 +12,7 @@ const Container = styled.div`
       rgba(255, 255, 255, 0.5),
       rgba(255, 255, 255, 0.5)
     ),
-    url("https://images.pexels.com/photos/6984661/pexels-photo-6984661.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940")
+    url("https://img.freepik.com/free-photo/nature-tranquil-beauty-reflected-calm-water-generative-ai_188544-12798.jpg")
       center;
   background-size: cover;
   display: flex;
