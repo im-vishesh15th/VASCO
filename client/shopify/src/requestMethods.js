3a27de49fbcb13772303cba26dd24cb08c3ac8d7
im-vishesh15th <145531271+im-vishesh15th@users.noreply.github.com>
2024-03-30 12:43:44 +0530

    Update requestMethods.js

--- client/shopify/src/requestMethods.js
+++ client/shopify/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://vasco-2.onrender.com/api/";
+const BASE_URL = "https://vasco-psi.vercel.app/api/";
 // const TOKEN =
 //   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
 //     .accessToken || "";
