2b3d48fd5dfcd5b9017f12efdcbcd44e2b79f418
im-vishesh15th <145531271+im-vishesh15th@users.noreply.github.com>
2024-06-25 01:04:47 +0530

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,21 +2,110 @@ const router = require("express").Router();
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const nodemailer = require('nodemailer');
+const crypto = require('crypto');
+const dotenv = require("dotenv");
+dotenv.config();
+
+let otpStore = {};
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MY_MAIL,
+    pass: process.env.MY_MAIL_SEC,
+  },
+});
+
+router.post('/send-otp', async (req, res) => {
+
+  const { email } = req.body;
+  try {
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
+    const otp = crypto.randomInt(100000, 999999).toString();
+    otpStore[email] = otp;
+
+    const mailOptions = {
+      from: process.env.MY_MAIL,
+      to: email,
+      subject: 'VASCO Account Password Reset OTP',
+      html: `
+      <p>Dear Customer,</p>
+      <p>We received a request to reset your password for your VASCO account. Please use the OTP below to verify your identity and reset your password:</p>
+      <h2>${otp}</h2>
+      <p>If you did not request this change, please ignore this email.</p>
+      <p>Thank you,</p>
+      <p>VASCO Team</p>
+    `,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error("Error sending email:", error);
+        return res.status(500).json({ success: false, message: 'Failed to send OTP' });
+      } else {
+        console.log("Email sent:", info.response);
+        res.json({ success: true, message: 'OTP sent' });
+      }
+    });
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
+
+router.post('/verify-otp', (req, res) => {
+  const { email, otp } = req.body;
+  if (otpStore[email] && otpStore[email] === otp) {
+    delete otpStore[email]; // OTP used, delete it from store
+    res.json({ success: true, message: 'OTP verified' });
+  } else {
+    res.status(400).json({ success: false, message: 'Invalid OTP' });
+  }
+});
+
+router.post('/reset', async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(406).json({ success: false, message: 'User not found' });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    user.password = hashedPassword;
+    await user.save();
+    console.log("hi");
+    res.json({ success: true, message: 'Password reset successful' });
+    console.log("hi");
+  } catch (err) {
+    console.log(err);
+    console.error("Error:", err);
+    res.status(500).json({ success: false, message: 'Internal server error' });
+  }
+});
 
 // REGISTER
 router.post("/register", async (req, res) => {
- 
-  if(!req.body.img)
-  {
-    req.body.img="https://picsum.photos/200/300";
+
+  if (!req.body.img) {
+    req.body.img = "https://picsum.photos/200/300";
   }
 
-  const { username, email, password ,img} = req.body;
+  const { username, email, password, img } = req.body;
 
   try {
     // Check if user already exists
-    if(req.body.password!==req.body.confirmPassword)
-    {
+    if (req.body.password !== req.body.confirmPassword) {
       return res.status(400).json({ error: "Confirm Password is Different... " });
     }
     const existingUser = await User.findOne({ username });
@@ -33,12 +122,12 @@ router.post("/register", async (req, res) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     // Create new user
-    const newUser = new User({ username, email, password: hashedPassword,img:img });
-    
+    const newUser = new User({ username, email, password: hashedPassword, img: img });
+
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
   } catch (err) {
-   
+
     res.status(500).json({ error: "Internal server eror" });
   }
 });
@@ -50,20 +139,20 @@ router.post("/login", async (req, res) => {
   try {
     // Find user by username
     const user = await User.findOne({ username });
-   
+
     if (!user) {
-      
+
       return res.status(401).json({ error: "Invalid username or password" });
     }
 
     // Compare passwords
-    
+
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
-      
+
       return res.status(401).json({ error: "Invalid username or password" });
     }
-    
+
 
     // Generate access token
     const accessToken = jwt.sign(
