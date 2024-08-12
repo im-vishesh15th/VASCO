6740497d294cb46017595efa0597323511e1cdbc
im-vishesh15th <145531271+im-vishesh15th@users.noreply.github.com>
2024-08-13 00:46:17 +0530

    Update requestMethods.js

--- client/shopify/src/requestMethods.js
+++ client/shopify/src/requestMethods.js
@@ -1,21 +1,19 @@
 import axios from "axios";
 
 const BASE_URL = "https://vasco-2.onrender.com/api/";
-// const TOKEN =
-//   JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser
-//     .accessToken || "";
-console.log(localStorage);
+
+
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 
 
 const currentUser = user && JSON.parse(user).currentUser;
 
 
-const accessToken = currentUser?currentUser.accessToken:"vishesh";
+const accessToken = currentUser?currentUser.accessToken:"";
 console.log("Token:", accessToken);
 localStorage.setItem("accessToken", accessToken);
 
-console.log(user);
+
 
 
 
